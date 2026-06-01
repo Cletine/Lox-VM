dcad2bf01263b66232681affc66ab2658c268321
Ryan M <matimu.ryan@gmail.com>
2026-06-01 15:58:40 -0400

    Added context Environment

---
+++ src/lox/environment.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+use inkwell::values::PointerValue;
+use inkwell::types::BasicTypeEnum;
+
+
+
+#[derive(Clone, Debug)]
+pub struct VariableInfo<'ctx> {
+    pub pointer: PointerValue<'ctx>,
+    pub ty: BasicTypeEnum<'ctx>,
+    pub is_mutable: bool,
+}
+
+pub struct Environment <'ctx> {
+
+    scope: Vec<Hashmap<String, Object<'ctx>>>,
+}
+
+impl<'ctx> Environmnet <'ctx> {
+    
+    pub fn new() -> Self {
+        Self {
+            scopes: vec![HashMap::new()],
+        }
+    }
+
+    pub fn enter_scope(&mut self) {
+        self.scopes.push(HashMap::new());
+    }
+
+    pub fn exit_scope(&mut self) {
+        if self.scopes.len() > 1 {
+            self.scopes.pop();
+        }
+        else {
+            panic!("Cannot pop global scopes.");
+        }
+    }
+
+    pub fn define(&mut self, name: String, pointer: PointerValue<'ctx>, ty: BasicTypeEnum, is_mutable: bool) {
+        if let Some(current_scope) = self.scopes.last_mut() {
+            let info = VariableInfo {pointer: pointer, ty:ty, is_mutable:is_mutable};
+            current_scope.insert(name, info);
+        }
+    }
+
+    pub fn lookup(&self, name: &str) -> Option<&VariableInfo<'ctx>> {
+        for scope in self.scopes.iter().rev() {
+            if let Some(info) = scope.get(name) {
+                return Some(info);
+            }
+        }
+        None
+    }
+}
+
+
