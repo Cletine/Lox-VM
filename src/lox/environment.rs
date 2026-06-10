13ba1283a8adf45b3a9d7c992630ba1821723f5b
Ryan M <matimu.ryan@gmail.com>
2026-06-10 15:49:24 -0400

    Updated environment function impl to include compiler lifetimes for attributes and richer error handling expressions

--- src/lox/environment.rs
+++ src/lox/environment.rs
@@ -1,6 +1,8 @@
-use std::collections::HashMap;
-use inkwell::values::PointerValue;
+use std::collections::HashMap; use inkwell::values::PointerValue;
 use inkwell::types::BasicTypeEnum;
+use crate::lox::Token;
+use crate::CompilerError;
+
 
 
 
@@ -12,58 +14,66 @@ pub struct VariableInfo<'ctx> {
 }
 
 pub struct Environment <'ctx> {
-
-    scope: Vec<Hashmap<&str, VariableInfo<'ctx>>>,
+    scope: Vec<HashMap<&'ctx str, VariableInfo<'ctx>>>,
 }
 
-impl<'ctx> Environmnet <'ctx> {
+impl<'ctx> Environment <'ctx> {
     
     pub fn new() -> Self {
         Self {
-            scopes: vec![HashMap::new()],
+            scope: vec![HashMap::new()],
         }
     }
 
-    pub fn enter_scope(&mut self) {
-        self.scopes.push(HashMap::new());
+    pub fn enter_scope(&mut self) -> Result<(), CompilerError> {
+        self.scope.push(HashMap::new());
+        return Ok(())
     }
 
-    pub fn exit_scope(&mut self) {
-        if self.scopes.len() > 1 {
-            self.scopes.pop();
+    pub fn exit_scope(&mut self) -> Result<(), CompilerError> {
+        if self.scope.len() > 1 {
+            self.scope.pop();
+            return Ok(())
         }
         else {
-            panic!("Internal Compiler Error: Mismatched scope lifecycle tracking.");
+            return Err(CompilerError::VariableEnvironment("Internal Compiler Error: Mismatched scope lifecycle tracking.".to_string()));
         }
     }
 
-    pub fn define(&mut self, name: &Token, pointer: PointerValue<'ctx>, ty: BasicTypeEnum, is_mutable: bool) {
-        let var_name = name.lexeme.to_str();
-        if let Some(current_scope) = self.scopes.last_mut() {
+
+    pub fn define(&mut self, name: &'ctx Token, pointer: PointerValue<'ctx>, ty: BasicTypeEnum<'ctx>, is_mutable: bool) -> Result<(), CompilerError>{
+        let var_name = name.lexeme.as_str();
+        if let Some(current_scope) = self.scope.last_mut() {
             let info = VariableInfo {pointer: pointer, ty:ty, is_mutable:is_mutable};
-            current_scope.insert(name, info);
+            current_scope.insert(var_name, info);
+            return Ok(())
+        }
+        else {
+            return Err(CompilerError::VariableEnvironment("Internal Compiler Error: Unable to Define Variable in the Current Scope.".to_string()));
         }
     }
 
-    pub fn lookup(&self, name: &Token) -> Option<&VariableInfo<'ctx>> {
-        let var_name = name.lexeme.to_str();
-        for scope in self.scopes.iter().rev() {
-            if let Some(info) = scope.get(name) {
-                return Some(info);
+
+    pub fn lookup(&mut self, name: &'ctx Token) -> Result<&VariableInfo<'ctx>, CompilerError> {
+        let var_name = name.lexeme.as_str();
+        for current_scope in self.scope.iter().rev() {
+            if let Some(info) = current_scope.get(var_name) {
+                return Ok(info)
             }
         }
-        None
+        return Err(CompilerError::UndefinedVariable(format!("Variable '{}' not found.", name.lexeme)))
     }
 
-    pub fn assign(&self, name: &Token, pointer: PointerValue<'ctx>, ty: BasicTypeEnum, is_mutable: bool) {
+    pub fn assign(&mut self, name: &'ctx Token, pointer: PointerValue<'ctx>, ty: BasicTypeEnum<'ctx>, is_mutable: bool) -> Result<(), CompilerError> {
         let info = VariableInfo {pointer: pointer, ty:ty, is_mutable:is_mutable};
-        let var_name = name.lexeme.to_str();
-        for current_scope in self.scopes.iter().rev() {
+        let var_name = name.lexeme.as_str();
+        for current_scope in self.scope.iter_mut().rev() {
             if current_scope.contains_key(var_name) {
-                return current_scope.insert(var_name, info);
+                current_scope.insert(var_name, info);
+                return Ok(())
             }
         }
-        panic!("Internal Compiler Error: Undefined Variable '{}'", name.lexeme);
+        return Err(CompilerError::UndefinedVariable(format!("Internal Compiler Error: Undefined Variable '{}'", name.lexeme)));
     }
 }
 
