48a7fc113a9b829bf8b0e4c184182232c5df1433
Ryan M <matimu.ryan@gmail.com>
2026-06-10 15:47:06 -0400

    Updated Stmt and Expr Visitor frameworks to include all AST node functions, variable environments, and compiler lifetime

--- src/lox/compiler.rs
+++ src/lox/compiler.rs
@@ -1,16 +1,21 @@
-//
 use inkwell::context::Context;
 use inkwell::module::Module;
 use inkwell::builder::Builder;
 use inkwell::values::BasicValueEnum;
-use inkwell::AddressSpace::default;
+use inkwell::values::FloatValue;
+use inkwell::values::IntValue;
+use inkwell::values::PointerValue;
+use inkwell::AddressSpace;
+use crate::lox::Token;
+use crate::lox::Object;
+use crate::lox::Expr;
+use crate::lox::Statement;
+use crate::lox::StmtVisitor;
+use crate::lox::ExprVisitor;
+use crate::CompilerError;
+use crate::lox::Environment;
+use crate::lox::VariableInfo;
 
-#[derive(Debug)]
-pub enum CompilerError {
-    LLVMError(String),
-    UndefinedVariable(String),
-    TypeMismatch(String),
-}
 
 pub struct CodeGen<'ctx> {
     pub context: &'ctx Context,
@@ -20,32 +25,86 @@ pub struct CodeGen<'ctx> {
     // also eventually put symbol table/scopes here
 }
 
-impl<'ctx> StmtVisitor<Result<(), CompilerError>> for CodeGen<'ctx> {
+impl<'ctx> StmtVisitor<'ctx, Result<(), CompilerError>> for CodeGen<'ctx> {
     // Implement statement compilation methods here (e.g., generating functions, blocks)
     // Most statement implementations will return Ok(()) as they emit instructions into the blocks
+    fn visit_block_stmt(&mut self, statements: &'ctx Vec<Box<Statement>>) -> Result<(), CompilerError> {
+        todo!()
+    }
 
+    fn visit_expr_stmt(&mut self, expression: &'ctx Expr) -> Result<(), CompilerError>  {
+        todo!()
+    }
+    fn visit_function_stmt(&mut self, name: &'ctx Token, arguments: &'ctx Vec<Box<Expr>>, body: &'ctx Vec<Box<Statement>>) -> Result<(), CompilerError> {
+        todo!()
+    }
+    fn visit_if_stmt(&mut self, condition: &'ctx Expr, then_branch: &'ctx Statement, else_branch: &'ctx Option<Statement>) -> Result<(), CompilerError> {
+        todo!()
+    }
 
+    fn visit_return_stmt(&mut self, keyword: &'ctx Token, value: &'ctx Expr) -> Result<(), CompilerError> {
+        todo!()
+    }
+
+    fn visit_while_stmt(&mut self, condition: &'ctx Expr, body: &'ctx Statement) -> Result<(), CompilerError> {
+        todo!()
+    }
+
+    fn visit_var_stmt(&mut self, name: &'ctx Token, initializer: &'ctx Expr) -> Result<(), CompilerError> {
+        todo!()
+    }
 }
 
-impl<'ctx> ExprVisitor<Result<inkwell::values::BasicValueEnum<'ctx>, CompilerError>> for CodeGen<'ctx> {
+impl<'ctx> ExprVisitor<'ctx, Result<inkwell::values::BasicValueEnum<'ctx>, CompilerError>> for CodeGen<'ctx> {
     // Implement expression compilation methods here
     // Most expressions will return Ok(BasicValueEnum) representing the computed LLVM value
-    //
+    fn visit_binary_expr(&mut self, left: &'ctx Expr, operator: &'ctx Token, right: &'ctx Expr) -> Result<BasicValueEnum<'ctx>, CompilerError>{
+        todo!()
+    }
 
-    fn visit_variable_expr(&mut self, name: &Token) {
-        match self.environment.lookup(&Token) {
-            Ok() => todo!(),
-            None => todo!(), 
-                //UndefinedVariable(format!("Undefined Variable: {}", name.lexeme)),
+
+    fn visit_call_expr(&mut self, callee: &'ctx Expr, paren: &'ctx Token, arguments: &'ctx Vec<Box<Expr>>) -> Result<BasicValueEnum<'ctx>, CompilerError>{
+        todo!()
+    }
+
+
+    fn visit_grouping_expr(&mut self, expression: &'ctx Expr) -> Result<BasicValueEnum<'ctx>, CompilerError>{
+        todo!()
+    }
+
+
+
+    fn visit_logical_expr(&mut self, left: &'ctx Expr, operator: &'ctx Token, right: &'ctx Expr) -> Result<BasicValueEnum<'ctx>, CompilerError>{
+        todo!()
+    }
+
+
+
+
+    fn visit_assign_expr(&mut self, name: &'ctx Token, value: &'ctx Expr) -> Result<BasicValueEnum<'ctx>, CompilerError>{
+        todo!()
+    }
+
+    fn visit_variable_expr(&mut self, name: &'ctx Token) -> Result<BasicValueEnum<'ctx>, CompilerError>{
+        match self.environment.lookup(name) {
+            Ok(VariableInfo {pointer: ptr, ty: t, is_mutable: mutbl}) => {
+               todo!() 
+            }
+            Err(compiler_error) =>  return Err(compiler_error),
         }
     }
 
-    fn visit_literal_expr(&mut self, value: &Object) -> Result<BasicValueEnum<'ctx>, CompilerError> {
+
+    fn visit_unary_expr(&mut self, operator: &'ctx Token, right: &'ctx Expr) -> Result<BasicValueEnum<'ctx>, CompilerError> {
+        todo!()
+    }
+
+    fn visit_literal_expr(&mut self, value: &'ctx Object) -> Result<BasicValueEnum<'ctx>, CompilerError> {
         match value {
-            Object::STRING(string) => literal_string_expr_node(string),
-            Object::NUMBER(number) => self.literal_number_expr_node(number),
+            Object::STRING(string) => self.literal_string_expr_node(string),
+            Object::NUMBER(number) => self.literal_number_expr_node(*number),
             Object::NULL => self.literal_null_expr_node(),
-            Object::BOOL(boolean) => self.literal_bool_expr_node(boolean),
+            Object::BOOL(boolean) => self.literal_bool_expr_node(*boolean),
         }
     }
 }
@@ -58,9 +117,9 @@ impl <'ctx> CodeGen<'ctx> {
     }
 
     fn literal_null_expr_node(&mut self) -> Result<BasicValueEnum<'ctx>, CompilerError> {
-        let ptr_type = self.context.ptr_type(default());
+        let ptr_type = self.context.ptr_type(AddressSpace::default());
         let null_ptr: PointerValue<'ctx> = ptr_type.const_null();
-        return Ok(BasicValueEnum::FloatValue(null_ptr))
+        return Ok(BasicValueEnum::PointerValue(null_ptr))
     }
 
     fn literal_bool_expr_node(&mut self, value: bool) -> Result<BasicValueEnum<'ctx>, CompilerError> {
@@ -69,25 +128,27 @@ impl <'ctx> CodeGen<'ctx> {
         return Ok(BasicValueEnum::IntValue(constant_bool))
     }
 
-    fn literal_string_expr_node(&mut self, value: String) -> Result<BasicValueEnum<'ctx>, CompilerError> {
-        let global_string_ptr = self.builder.build_global_string_ptr(value.as_str(),"str_literal")
+    fn literal_string_expr_node(&mut self, value: &str) -> Result<BasicValueEnum<'ctx>, CompilerError> {
+        let global_string_ptr = self.builder.build_global_string_ptr(value,"str_literal")
             .map_err(|_| CompilerError::LLVMError("Failed to build global string".to_string()))?;
         let basic_value: BasicValueEnum<'ctx> = global_string_ptr.as_pointer_value().into();
-        Ok(basic_value)
+        return Ok(basic_value)
     }
 }
 
 
-pub fn compile_program(program: &[Box<Statement>]) -> Result<(), CompilerError> {
+pub fn compile_program(program: &Vec<Statement>, file: &str) -> Result<(), CompilerError> {
     let context = Context::create();
-    let module = context.create_module("my_program");
+    let module = context.create_module(file);
     let builder = context.create_builder();
+    let environment = Environment::new();
 
     // Instantiate your code generator pass
     let mut codegen = CodeGen {
         context: &context,
         module,
         builder,
+        environment,
     };
 
     // Process the list of statements sequentially
