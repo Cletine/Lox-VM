d01ca4e50d7be420b1717261783babb027407b9d
Ryan M <matimu.ryan@gmail.com>
2026-06-10 15:52:36 -0400

    Updated mod.rs to include environment modules

--- src/lox/mod.rs
+++ src/lox/mod.rs
@@ -3,12 +3,16 @@ pub mod scanner;
 mod token;
 mod expressions;
 pub mod parser;
+pub mod compiler;
+pub mod environment;
 
-pub use self::expressions::{Expr, Statement};
+pub use self::expressions::{Expr, Statement, ExprVisitor, StmtVisitor};
 pub use self::object::Object;
 pub use self::scanner::LoxScanner;
 pub use self::token::{Token, TokenType};
 pub use self::parser::LoxParser;
+pub use self::environment:: {VariableInfo, Environment};
+
 
 #[cfg(test)]
 mod tests;
